9b0f75b2b38671d4be89ada8f15d0a20d838d457
koll001 <oasis3150@gmail.com>
2021-04-14 14:19:27 +0900

    課題内容実装

--- src/store/index.js
+++ src/store/index.js
@@ -23,74 +23,90 @@ export default new Vuex.Store({
   mutations: {
     setUser(state, val) {
       state.user = val;
-      console.log(val);
     },
     setUserBalance(state, val) {
       state.userBalance = val;
-      console.log(val);
     },
   },
   actions: {
     async signUpUser({ commit }, { email, password, userName }) {
       const auth = firebase.auth();
-      const result = await auth.createUserWithEmailAndPassword(email, password);
-      const user = result.user;
-      await user.updateProfile({
-        displayName: userName,
-      });
-      commit('setUser', user);
-      const database = firebase.database();
-      await database
-        .ref('users')
-        .child(user.uid)
-        .set({
-          userName: user.displayName,
-          email: user.email,
-          balance: 1000,
+      try {
+        const result = await auth.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        const user = result.user;
+        await user.updateProfile({
+          displayName: userName,
         });
-      commit('setUserBalance', 1000);
-      router.push('/home');
+        commit('setUser', user);
+        const database = firebase.database();
+        await database
+          .ref('users')
+          .child(user.uid)
+          .set({
+            userName: user.displayName,
+            email: user.email,
+            balance: 1000,
+          });
+        commit('setUserBalance', 1000);
+        router.push('/home');
+      } catch (error) {
+        console.log(error);
+      }
     },
     async loginUser({ commit }, { email, password }) {
       const auth = firebase.auth();
-      const result = await auth.signInWithEmailAndPassword(email, password);
-      const user = result.user;
-      commit('setUser', user);
-      const database = firebase.database();
-      database
-        .ref('users')
-        .child(user.uid)
-        .on('value', (snapshot) => {
-          const data = snapshot.val();
-          const userBalance = data.balance;
-          commit('setUserBalance', userBalance);
-        });
-      router.push('/home');
+      try {
+        const result = await auth.signInWithEmailAndPassword(email, password);
+        const user = result.user;
+        commit('setUser', user);
+        const database = firebase.database();
+        database
+          .ref('users')
+          .child(user.uid)
+          .on('value', (snapshot) => {
+            const data = snapshot.val();
+            const userBalance = data.balance;
+            commit('setUserBalance', userBalance);
+          });
+        router.push('/home');
+      } catch (error) {
+        console.log(error);
+      }
     },
 
     async signOutUser() {
       const auth = firebase.auth();
-      await auth.signOut();
-      router.push('/signin').catch((err) => {
-        if (
-          err.name !== 'NavigationDuplicated' &&
-          !err.message.includes(
-            'Avoided redundant navigation to current location'
-          )
-        ) {
-          // But print any other errors to the console
-          console.log(err);
-        }
-      });
+      try {
+        await auth.signOut();
+        router.push('/signin').catch((error) => {
+          if (
+            error.name !== 'NavigationDuplicated' &&
+            !error.message.includes(
+              'Avoided redundant navigation to current location'
+            )
+          ) {
+            console.log(error);
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
 
     async checkUserLoggedIn() {
       const auth = firebase.auth();
-      auth.onAuthStateChanged((user) => {
-        if (!user) {
-          router.push('/signin');
-        }
-      });
+      try {
+        auth.onAuthStateChanged((user) => {
+          if (!user) {
+            router.push('/signin');
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 });
